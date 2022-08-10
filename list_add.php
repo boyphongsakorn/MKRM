8f8154561466e440f22e8406a9c209c059a31639
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-10 21:07:59 +0700

    test

--- list_add.php
+++ list_add.php
@@ -7,6 +7,8 @@ $sql = "INSERT INTO list VALUES (NULL, '".$_POST['name']."', '".$_POST['nickname
 
 if ($mysqli->query($sql) === TRUE) {
   echo "1";
+  //alert success and location to https://mail-list-three.vercel.app/
+  echo "<script>alert('성공적으로 추가되었습니다.'); location.href='https://mail-list-three.vercel.app/';</script>";
 } else {
   echo "Error: " . $sql . "<br>" . $mysqli->error;
 }
