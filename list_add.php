d00309b0f1fa3754dcbd1cf46d8bbfaeaba28621
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-12 14:28:41 +0700

    test

--- list_add.php
+++ list_add.php
@@ -3,7 +3,7 @@ include 'config.php';
 
 $mysqli -> select_db("mail_list");
 
-$sql = "INSERT INTO list VALUES (NULL, '".$_POST['name']."', '".$_POST['nickname']."', '".$_POST['address']."')";
+$sql = "INSERT INTO list VALUES (NULL, '".$_POST['name']."', '".$_POST['nickname']."', '".$_POST['address']."',0)";
 
 if ($mysqli->query($sql) === TRUE) {
   echo "1";
