3e3fc87c9b147d53e623288a50e40f79d98b19cb
Phongsakorn Wisetthon <boy1556@hotmail.com>
2024-01-08 10:44:56 +0700

    Fix SQL query in oldlist.php

--- oldlist.php
+++ oldlist.php
@@ -3,7 +3,7 @@ include 'config.php';
 
 $mysqli -> select_db("mkrm");
 
-$sql = "SELECT max(pl_ytname),pl_ytid FROM playlist where pl_ytname <> 'test' group by pl_ytid order by max(pl_datetime) desc";
+$sql = "SELECT max(pl_ytname) as pl_ytname,pl_ytid FROM playlist where pl_ytname <> 'test' group by pl_ytid order by max(pl_datetime) desc";
 
 if ($result = $mysqli->query($sql)) {
     //echo json
